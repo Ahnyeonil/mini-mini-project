50b81488f1ccc0371e76da9347948354020f9615
kimyuntae19 <kimyuntae19@gmail.com>
2022-08-18 09:44:32 +0900

    sign/login base

--- app.py
+++ app.py
@@ -1,31 +1,78 @@
-# 패키지 설치 -> flask, pymongo, dnspython
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
-from flask import Flask, render_template, request, jsonify
+client = MongoClient('mongodb+srv://test:sparta@cluster0.38yzx.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 app = Flask(__name__)
 
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://ahn:sparta@cluster0.s9tldtb.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbprestudy
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
 @app.route("/login")
 def login_page():
     return render_template('login.html')
 
-@app.route("/login", methods=["POST"])
-def doLogin():
-    return jsonify({'msg': 'POST 연결 완료!'})
 
-@app.route("/logout")
-def doLogOut():
-    return jsonify({'msg': 'POST 연결 완료!'})
+@app.route("/login", methods=["POST"])
+def do_login():
+    id_receive = request.form['member_id']
+    pw_receive = request.form['member_pw']
+
+    user_id = db.members.find_one({'memberid': id_receive}, {'_id': False})
+    user_pw = db.members.find_one({'memberid': id_receive, 'memberpw': pw_receive}, {'_id': False})
+
+    if user_id is None:
+        return jsonify({'msg': '존재 하지 않는 아이디 입니다'})
+    elif user_pw is None:
+        return jsonify({'msg': '비밀 번호가 다릅니다'})
+    else:
+        user_name = db.members.find_one({'memberid': id_receive}, {'_id': False})['name']
+        return jsonify({'msg': '로그인 완료!', 'member': user_name})
+
+
+@app.route("/logout", methods=["GET"])
+def do_logout():
+    return jsonify({'msg': '로그 아웃 완료!'})
+
 
 @app.route("/new_member")
-def newMember_page():
+def new_member_page():
     return render_template('new_member.html')
+
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    userid_receive = request.form['userid']
+    userpw_receive = request.form['userpw']
+    username_receive = request.form['username']
+    usernickname_receive = request.form['usernickname']
+
+    doc = {
+        'memberid': userid_receive,
+        'memberpw': userpw_receive,
+        'name': username_receive,
+        'nickname': usernickname_receive
+    }
+
+    doc_id = doc['memberid']
+    doc_nickname = doc['nickname']
+
+    members = list(db.members.find({}, {'_id': False}))
+
+    for mem in members:
+        mem_id = mem['memberid']
+        mem_nickname = mem['nickname']
+        if doc_id == mem_id:
+            return jsonify({'msg': '동일한 id가 존재합니다!'})
+        elif doc_nickname == mem_nickname:
+            return jsonify({'msg': '동일한 닉네임이 존재합니다!'})
+
+    db.members.insert_one(doc)
+    return jsonify({'msg': '회원 가입 완료!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/common.js
+++ static/js/common.js
@@ -5,3 +5,128 @@ window.onload=function(){
 let goBack = function() {
     window.history.back();
 };
+
+function loginBtnOn() {
+    $('#logoutBtn').css('display', 'none');
+    $('#signUpBtn').css('display', 'inline-block');
+    $('#loginBtn').css('display', 'inline-block');
+}
+
+function loginBtnOut() {
+    $('#loginBtn').css('display', 'none');
+    $('#signUpBtn').css('display', 'none');
+    $('#logoutBtn').css('display', 'inline-block');
+}
+
+function login() {
+
+    let login_id = $('#member_id').val()
+    let login_pw = $('#member_pw').val()
+
+    $.ajax({
+        type: "POST",
+        url: "/login",
+        data: {member_id: login_id, member_pw: login_pw},
+        success: function (response) {
+
+            console.log(response)
+
+            let membername = response['member']
+            let html_temp = `<div id="visit-comment">${membername}님 환영합니다</div>`
+
+            if (response['msg'] === '로그인 완료!') {
+                loginBtnOut()
+                $('.form-group').hide()
+                alert(response['msg'])
+                $('#loginMember').prepend(html_temp)
+            }
+            else {
+                loginBtnOn()
+                alert(response['msg'])
+            }
+
+        }
+    })
+}
+
+function logout() {
+
+    $.ajax({
+        type: "GET",
+        url: "/logout",
+        data: {},
+        success: function (response) {
+            console.log(response)
+            alert(response['msg'])
+            loginBtnOn()
+            $('.form-group').show()
+            $('#visit-comment').remove()
+        }
+    })
+}
+
+function signup() {
+
+    let user_id = $('#member_id').val()
+    let user_pw = $('#member_pw').val()
+    let pw_confirm = $('#member_pw_').val()
+    let user_name = $('#member_name').val()
+    let user_nickname = $('#member_nickname').val()
+
+    // 유효성 검사 옵션
+    if (user_id === "") {
+        $('#member_id').focus()
+        return alert("아이디를 입력하세요")
+    }
+    if(user_pw === ""){
+        $('#member_pw').focus()
+        return alert("비밀번호를 입력하세요")
+    }
+    if (user_pw !== pw_confirm) {
+        $('#member_pw_').val('')
+        $('#member_pw_').focus()
+        return alert("비밀번호가 다릅니다")
+    }
+    let pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*\d).{8,25}$/;
+    if (!pwdCheck.test(user_pw)) {
+        $('#member_pw').val('')
+        $('#member_pw_').val('')
+        $('#member_pw').focus()
+        return alert("비밀번호는 영문자+숫자+특수문자 조합으로 8~25자리 사용해야 합니다")
+    }
+    if (user_name === ""){
+        $('#member_name').focus()
+        return alert("이름을 입력하세요")
+    }
+    if (user_nickname === ""){
+        $('#member_nickname').focus()
+        return alert("닉네임을 입력하세요")
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/signup",
+        data: {
+            userid: user_id,
+            userpw: user_pw,
+            username: user_name,
+            usernickname: user_nickname
+        },
+        success: function (response) {
+
+            if (response['msg'] === '회원 가입 완료!'){
+                alert(response['msg'])
+                location.replace('/login')
+            }
+            else if (response['msg'] === '동일한 id가 존재합니다!') {
+                alert(response['msg'])
+                $('#member_id').val('')
+            }
+            else if (response['msg'] === '동일한 닉네임이 존재합니다!') {
+                alert(response['msg'])
+                $('#member_nickname').val('')
+            }
+
+        }
+    })
+}
